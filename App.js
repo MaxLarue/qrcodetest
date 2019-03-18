2e9edef55c35e8b1e62394822c5735e4b1ffe699
MaxLarue <maximilienlarue@gmail.com>
2019-03-18 19:33:15 +0100

    works on android

---
+++ App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
+
+export default class ScanScreen extends Component {
+
+  state={
+    showScanner: true
+  }
+
+  onSuccess(e) {
+    alert(e.data);
+    this.setState({showScanner: false})
+    var self = this
+    setTimeout(() => { self.setState({showScanner: true}) }, 2000)
+  }
+
+
+  render() {
+    if(this.state.showScanner)
+    return (
+      <QRCodeScanner
+        onRead={this.onSuccess.bind(this)}
+        topContent={
+          <Text style={styles.centerText}>
+            Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+          </Text>
+        }
+        bottomContent={
+          <TouchableOpacity style={styles.buttonTouchable}>
+            <Text style={styles.buttonText}>OK. Got it!</Text>
+          </TouchableOpacity>
+        }
+      />
+    );
+    return null
+  }
+}
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
